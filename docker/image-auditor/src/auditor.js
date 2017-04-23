a68fbfb4f01f9adfd4535b410adf603ece20c656
luca15 <luca.sivillica@hotmail.com>
2017-04-23 18:08:20 +0200

    Labo UDP done and all the tests passed

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,17 +1,44 @@
 /**
- * This script
+ * This script allows create an auditor.
+ * The auditor listens to musicians that play instruments, he listens to
+ * musicians by using the protocol UDP by joining a multicast group.
+ * The auditor considers that if a musician didn't play since 5 seconds,
+ * he's dead.
+ * The auditor can be joined by TCP and he just retruns a JSON array that
+ * contains all the musicians that are alive.
+ * An example of the contents of the JSON array is :
+ * [
+ *  {
+ *	 "uuid" : "aa7d8cb3-a15f-4f06-a0eb-b8feb6244a60",
+ *	 "instrument" : "piano",
+ *	 "activeSince" : "2016-04-27T05:20:50.731Z"
+ *  },
+ *  {
+ *	 "uuid" : "06dbcbeb-c4c8-49ed-ac2a-cd8716cbf2d3",
+ *	 "instrument" : "flute",
+ *	 "activeSince" : "2016-04-27T05:39:03.211Z"
+ *  }
+ * ]
+ *
  *
  * author : Luca Sivillica
  * date   : 19.04.2017
  */
 
+//------------------------------------------------------------------------------
+// Loading extern modules
 
 var schedule = require("node-schedule");
-var moment = require("moment");
-var	dgram	=	require("dgram");
-var net = require("net");
+var moment   = require("moment");
+var	dgram	   = require("dgram");
+var net      = require("net");
+
+//------------------------------------------------------------------------------
+// Declaration of variables and constants
+
 var	socketUDP	=	dgram.createSocket("udp4");
 var socketTCP;
+var auditor;   // the auditor who listens to musicians
 
 var soundInstruments = new Map([
   ["ti-ta-ti" , "piano"],
@@ -21,11 +48,20 @@ var soundInstruments = new Map([
   ["boum-boum", "drum"]
 ]);
 
+const ProtocolUDP = {
+  PORT              : 7777,
+  MULTICAST_ADDRESS : "239.255.22.5"
+};
+
+const ProtocolTCP = {
+  PORT : 2205
+};
+
 //------------------------------------------------------------------------------
 // Declaration of functions and classes
 
 /**
- * class Musician
+ * Class that represents a musician
  */
 function Musician(uuid, instrument, activeSince) {
   this.uuid = uuid;
@@ -34,34 +70,45 @@ function Musician(uuid, instrument, activeSince) {
 }
 
 /**
- * class Auditor
+ * Class that represents an auditor
  */
  function Auditor() {
-   var listMusicians = new Map();
 
+   var listMusicians = new Map(); // list of musicians alive
+
+   /**
+    * This function allows to add a musician in the list.
+    */
    this.addMusician = function(musician) {
-     var musicianObject = new Musician(musician.uuid,
-                                       soundInstruments.get(musician.sound),
-                                       Date.now());
-
-     listMusicians.set(musician.uuid, musicianObject);
+     listMusicians.set(musician.uuid, musician);
    }
 
+   /**
+    * This function allows to remove a musician from the list.
+    */
    var removeMusician = function(musician) {
      listMusicians.delete(musician.uuid);
    }
 
+   /**
+    * This function checks if a musician is active.
+    * A musician is active if he has played a sound during the last 5 seconds.
+    */
    var isMusicianActive = function(musician) {
      var musicianObject = listMusicians.get(musician.uuid);
 
+     /* if the musician is in the list, we check if he's active */
      if (typeof musicianObject !== "undefined") {
-       return Date.now() - musicianObject.activeSince <= 5000;
+       return Date.now() - musicianObject.activeSince <= 5000; // time in ms
      }
 
      return false;
    }
 
-   this.removeUnactiveMusician = function() {
+   /**
+    * This function remove all the unactive musicians.
+    */
+   this.removeUnactiveMusicians = function() {
      for (var musician of listMusicians.values()) {
        if (!isMusicianActive(musician)) {
          removeMusician(musician);
@@ -69,11 +116,14 @@ function Musician(uuid, instrument, activeSince) {
      }
    }
 
-   this.getArrayMusician = function() {
+   /**
+    * This function returns an array with all the musicians in the list.
+    */
+   this.getArrayMusicians = function() {
      var arrayMusician = [];
 
      for (var musician of listMusicians.values()) {
-       musician.activeSince = moment(musician.activeSince);
+       musician.activeSince = moment(musician.activeSince); // Formate time
 
        arrayMusician.push(musician);
      }
@@ -82,30 +132,40 @@ function Musician(uuid, instrument, activeSince) {
    }
  }
 
-const Protocol = {
-   PORT              : 7777,
-   MULTICAST_ADDRESS : "239.255.22.5"
- };
+//------------------------------------------------------------------------------
+// Main Script
 
-var auditor = new Auditor();
+auditor = new Auditor();
 
-socketUDP.bind(Protocol.PORT,	function() {
-  socketUDP.addMembership(Protocol.MULTICAST_ADDRESS);
+/* create the socket UDP and join the multicast group */
+socketUDP.bind(ProtocolUDP.PORT,	function() {
+  socketUDP.addMembership(ProtocolUDP.MULTICAST_ADDRESS);
 });
 
-//	This	call	back	is	invoked	when	a	new	datagram	has	arrived.
+/* when a new datagram has arrived, we add the musician in the auditor */
 socketUDP.on("message",	function(msg,	source)	{
-  auditor.addMusician(JSON.parse(msg.toString()));
+  var musicianFromUDP = JSON.parse(msg.toString());
+
+  var musician = new Musician(musicianFromUDP.uuid,
+                              soundInstruments.get(musicianFromUDP.sound),
+                              Date.now());
+
+  auditor.addMusician(musician);
 });
 
+/* create the server TCP and when a segment TCP has arrived, he send to the
+   the client an array of all the musicians alive */
 socketTCP = net.createServer(function(socket) {
-  socket.write(JSON.stringify(auditor.getArrayMusician()));
+  socket.write(JSON.stringify(auditor.getArrayMusicians()));
   socket.end();
 });
 
-socketTCP.listen(2205);
+socketTCP.listen(ProtocolTCP.PORT);
 
-var rule = new schedule.RecurrenceRule();
-rule.second = new schedule.Range(0, 59, 6);
+/* create a rule that must to be execute every 3 seconds */
+var rule    = new schedule.RecurrenceRule();
+rule.second = new schedule.Range(0, 59, 3);
 
-schedule.scheduleJob(rule, auditor.removeUnactiveMusician);
+/* we use this rule to remove every 3 seconds the unactive
+   musicians from the auditor */
+schedule.scheduleJob(rule, auditor.removeUnactiveMusicians);
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,5 +1,12 @@
 /**
- * This script
+ * This script allows create a musician with an instrument which is passed
+ * in a parameter on the script.
+ * The musician plays his instrument every 3 seconds which is represented by
+ * sending a datagram UDP to a multicast address ip.
+ * The contents of the datagram UDP is a JSON string with this structure :
+ *
+ * {"uuid" : Musician.uuid, "sound" : Musician.instrument.sound}.
+ *
  *
  * author : Luca Sivillica
  * date   : 19.04.2017
@@ -10,7 +17,7 @@
 // Declaration of functions and classes
 
 /**
- * class Instrument
+ * Class that represents an instrument
  */
 function Instrument(name, sound) {
   this.name = name;
@@ -18,7 +25,7 @@ function Instrument(name, sound) {
 }
 
 /**
- * class Musician
+ * Class that represents a musician
  */
 function Musician(uuid, instrument) {
   this.uuid = uuid;
@@ -26,7 +33,8 @@ function Musician(uuid, instrument) {
 }
 
 /**
- * class Instrument
+ * Function UUID generator
+ * source code from : https://gist.github.com/jcxplorer/823878
  */
 function uuid() {
   var uuid = "", i, random;
@@ -46,16 +54,17 @@ function uuid() {
 
 var schedule = require("node-schedule");
 var dgram    = require("dgram");
-var socket   = dgram.createSocket("udp4");
 
 //------------------------------------------------------------------------------
 // Declaration of variables and constants
 
-var playload;
-var message;
-var instrument;
-var argument;
-var musician;
+var socket   = dgram.createSocket("udp4");
+
+var playload;   // string JSON sent by UDP
+var message;    // buffer that contains the string JSON sent by UDP
+var instrument; // the instrument of the musician
+var argument;   // the argument that is passed on the script
+var musician;   // the musician who plays an instrument
 
 const Protocol = {
   PORT              : 7777,
@@ -64,6 +73,7 @@ const Protocol = {
 
 const INSTRUMENTS = ["piano", "trumpet", "flute", "violin", "drum"];
 
+/* instruments sound */
 var Sounds = {
   piano   : "ti-ta-ti",
   trumpet : "pouet",
@@ -75,7 +85,7 @@ var Sounds = {
 //------------------------------------------------------------------------------
 // Main Script
 
-/* if the argument wasn't given, we print an error and exit the programm */
+/* if the argument wasn't given, we print an error and exit the program */
 if (typeof process.argv[2] === "undefined") {
   console.log("Error: missing argument for instrument");
   return;
@@ -83,23 +93,29 @@ if (typeof process.argv[2] === "undefined") {
 
 argument = process.argv[2].toLowerCase();
 
-/* if the argument doesn't match with any instruments */
+/* if the argument doesn't match with any instruments then we print an error
+   and we exit the program, else we create the instrument */
 if (~INSTRUMENTS.indexOf(argument)) {
   instrument = new Instrument(argument, Sounds[argument]);
 
 } else {
+
   console.log("Error: instrument \"" + argument + "\" doesn't exist");
   return;
 }
 
 musician = new Musician(uuid(), instrument);
 
+/* create the JSON string sent by datagram UDP */
 playload = JSON.stringify({uuid : musician.uuid, sound : musician.instrument.sound});
-message = new Buffer(playload);
+message  = new Buffer(playload);
 
-var rule = new schedule.RecurrenceRule();
+/* create a rule that must to be execute every 3 seconds */
+var rule    = new schedule.RecurrenceRule();
 rule.second = new schedule.Range(0, 59, 3);
 
+/* we use this rule to send every 3 seconds a datagram UDP
+   which contains the JSON string */
 schedule.scheduleJob(rule, function() {
   socket.send(message, 0, message.length,	Protocol.PORT, Protocol.MULTICAST_ADDRESS);
 });
